0a3678192d7d745b533735967739a115a86029dc
andradean <viniciusansilva@outlook.com>
2023-10-31 08:49:26 -0300

    padrao DTO para users

--- src/main/java/com/vinicius/webservice/resources/UserResource.java
+++ src/main/java/com/vinicius/webservice/resources/UserResource.java
@@ -3,6 +3,7 @@ package com.vinicius.webservice.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vinicius.webservice.domain.User;
+import com.vinicius.webservice.dto.UserDTO;
 import com.vinicius.webservice.services.UserService;
 
 @RestController
@@ -21,11 +23,13 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 		List<User> list = service.findAll();
-
-		return ResponseEntity.ok().body(list);
+		
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
 
 	}
 }
